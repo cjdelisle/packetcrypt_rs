d3455e11fdd8b412328182090c2da39b11942d0a
Caleb James DeLisle <cjd@cjdns.fr>
2020-11-09 04:42:04 +0100

    Updated to fixed leak detector and use leak detector because otherwise it doesn't take out the correct lock

--- src/alloc.rs
+++ src/alloc.rs
@@ -65,11 +65,8 @@ fn write_mem_allocations(ld_size: usize, outfile: String) -> Result<()> {
                 )?;
             }
             AllocEnt::Frame(addr) => {
-                let mut ret: Option<String> = None;
-                backtrace::resolve(addr as *mut _, |symbol| {
-                    ret = Some(format!("\t0x{:x}: {:?}\n", addr, symbol));
-                });
-                file.write_all(&ret.unwrap().into_bytes()[..])?;
+                let name = unsafe { LEAK_TRACER.get_symbol_name(addr) };
+                file.write_all(&format!("\t0x{:x}: {:?}\n", addr, name).into_bytes()[..])?;
             }
         }
     }
