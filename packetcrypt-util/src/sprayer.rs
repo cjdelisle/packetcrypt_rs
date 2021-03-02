5f48ee8b51c4a4b8a9eb93b075bfe6d74c7c2b76
Caleb James DeLisle <cjd@cjdns.fr>
2021-03-02 21:14:31 +0100

    Bigger incoming buffer

--- packetcrypt-util/src/sprayer.rs
+++ packetcrypt-util/src/sprayer.rs
@@ -34,7 +34,8 @@ const SECONDS_UNTIL_RESUB: usize = 5;
 
 const STATS_EVERY: usize = 10;
 
-const RECV_BUF_SZ: usize = 1 << 26;
+// 128M incoming buffer
+const RECV_BUF_SZ: usize = 128 * 1024 * 1024;
 
 pub const MSG_PREFIX: usize = 8;
 pub const MSG_TOTAL_LEN: usize = 1024 + MSG_PREFIX;
