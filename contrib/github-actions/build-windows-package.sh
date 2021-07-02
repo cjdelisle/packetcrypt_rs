c1481fe20b2badaa5ee962b099a38d140b9fa46d
Thierry Marianne <thierry@marianne.io>
2021-07-02 18:44:30 +0200

    Pass portable flag to cargo build

--- contrib/github-actions/build-windows-package.sh
+++ contrib/github-actions/build-windows-package.sh
@@ -2,7 +2,7 @@
 
 function build() {
   cd "${GITHUB_WORKSPACE}" || exit
-  cargo build --release --target x86_64-pc-windows-gnu
+  cargo build --release --features portable --target x86_64-pc-windows-gnu
 
   mkdir ./bin
 
